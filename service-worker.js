f45cc715e6926c82031ed9c60891e3a609004200
Carl Bush <CarlBushs13@gmail.com>
2022-06-04 11:40:03 -0700

    functional offline mode

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const APP_PREFIX = "FoodFest-";
-const VERSION = "version_01";
+const APP_PREFIX = 'FoodFest1-';
+//const APP_PREFIX = 'FoodEvent-';
+const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 
@@ -15,16 +16,59 @@ const FILES_TO_CACHE = [
     "./dist/events.bundle.js",
     "./dist/tickets.bundle.js",
     "./dist/schedule.bundle.js"
-  ];
+];
 
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
 
-self.addEventListener("install", function (e) {
-    //e.waitUntil tells the browser to wait until work is complete before terming the service worker
-    e.wait.Until(
-        //caches.open used to find the specific cache by name then add every file to the FILES_TO_CACHE Array
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log("installing cache :" + CACHE_NAME);
-            return cache.addAll(FILES_TO_CACHE);
+            // You can omit if/else for console.log & put one line below like this too.
+            // return request || fetch(e.request)
         })
     )
+})
+
+//e.waitUntil tells the browser to wait until work is complete before terming the service worker
+//caches.open used to find the specific cache by name then add every file to the FILES_TO_CACHE Array
+// Cache resources
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        //.keys() returns an array of all cache names
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
 });
+
